91508df4c187f2ddcf2bd6a93a92683ad1b0b97c
Muhammad ElHelaly <mae@leanpeach.com>
2021-02-20 04:58:28 +0200

    Allow admin to delete an user

---
+++ UserManagementWithIdentity/Controllers/Api/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using UserManagementWithIdentity.Models;
+
+namespace UserManagementWithIdentity.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+                return NotFound();
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (!result.Succeeded)
+                throw new Exception();
+
+            return Ok();
+        }
+    }
+}
